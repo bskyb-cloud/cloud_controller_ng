bskyb-cloud/cloud_controller_ng

commit e59ea7bf064ef231c7a0908a05c13840621a45a9

Authenticate requests to list_handles
  This commit makes the following API end point authenticated:
GET /services/v1/offerings/:label/(:provider/)handles

Change-Id: I49f9a85901b88e7a484827e024377605540dd617

lib/cloud_controller/legacy_api/legacy_service_gateway.rb
@@ -79,6 +79,7 @@ module VCAP::CloudController
 
       service = Models::Service[:label => label, :provider => provider]
       raise ServiceNotFound, "label=#{label} provider=#{provider}" unless service
+      validate_access("#{label}-#{version}", provider)
       logger.debug("Listing handles for service: #{service.inspect}")
 
       handles = []

spec/api/legacy_service_gateway_spec.rb
@@ -229,26 +229,36 @@ module VCAP::CloudController
 
       describe "GET services/v1/offerings/:label(/:provider)/handles" do
         it "should return not found for unknown services" do
-          get "services/v1/offerings/foo-bar/handles"
+          get "services/v1/offerings/xxx-bar/handles"
           last_response.status.should == 404
         end
 
         it "should return not found for unknown services with a provider" do
-          get "services/v1/offerings/foo-bar/fooprovider/handles"
+          get "services/v1/offerings/xxx-bar/fooprovider/handles"
           last_response.status.should == 404
         end
 
-        it "should return provisioned and bound handles" do
+        before :each do
           svc1 = Models::Service.make(
             :label => "foo",
             :version => "bar",
             :provider => "core",
+          )
+          Models::ServiceAuthToken.create(
+            :label => "foo-bar",
+            :provider => "core",
+            :token => "foobar",
           )
 
           svc2 = Models::Service.make(
             :label    => "foo",
             :version  => "bar",
             :provider => "test",
+          )
+          Models::ServiceAuthToken.create(
+            :label => "foo-bar",
+            :provider => "test",
+            :token => "footest",
           )
 
           plan1 = Models::ServicePlan.make(
@@ -297,8 +307,19 @@ module VCAP::CloudController
             :gateway_name  => "bind2",
             :service_instance  => cfg2,
           )
+        end
 
-          get "/services/v1/offerings/foo-bar/handles"
+        it "rejects requests with mismatching tokens" do
+          get "/services/v1/offerings/foo-bar/handles", {}, {
+            "HTTP_X_VCAP_SERVICE_TOKEN" => "xxx",
+          }
+          last_response.status.should == 403
+        end
+
+        it "should return provisioned and bound handles" do
+          get "/services/v1/offerings/foo-bar/handles", {}, {
+            "HTTP_X_VCAP_SERVICE_TOKEN" => "foobar",
+          }
           last_response.status.should == 200
 
           handles = JSON.parse(last_response.body)["handles"]
@@ -308,7 +329,9 @@ module VCAP::CloudController
           handles[1]["service_id"].should == "bind1"
           handles[1]["configuration"].should == { "config" => "bind1" }
 
-          get "/services/v1/offerings/foo-bar/test/handles"
+          get "/services/v1/offerings/foo-bar/test/handles", {}, {
+            "HTTP_X_VCAP_SERVICE_TOKEN" => "footest",
+          }
           last_response.status.should == 200
 
           handles = JSON.parse(last_response.body)["handles"]

commit 8d81fe339f2163a26548d3364cb59d14476ab9e1

[fix] Fix list_handles authentication
  This commit fixes a bug introduced in commit e59ea7b where the service
auth token was located with both labels and versions. The correct
behavior is to match +create_offering+ , where service auth tokens are
only identified by labels. Corresponding variable names are also changed
to reflect the de-normalized content.

  Test plans:
    - all unit tests passed.
    - Manual verification:
      - deploy ccng prior to this commit, verify that
        "vmc create-service" fails
      - deploy this commit of ccng and verify that
        "vmc create-service" now works
    - BVT's passed.

Change-Id: I0e01da9ee004e28afc9e77fe9eb658b4ac35420b

lib/cloud_controller/legacy_api/legacy_service_gateway.rb
@@ -74,12 +74,12 @@ module VCAP::CloudController
       empty_json
     end
 
-    def list_handles(label, provider = DEFAULT_PROVIDER)
-      (label, version) = label.split("-")
+    def list_handles(label_and_version, provider = DEFAULT_PROVIDER)
+      (label, version) = label_and_version.split("-")
 
       service = Models::Service[:label => label, :provider => provider]
       raise ServiceNotFound, "label=#{label} provider=#{provider}" unless service
-      validate_access("#{label}-#{version}", provider)
+      validate_access(label, provider)
       logger.debug("Listing handles for service: #{service.inspect}")
 
       handles = []
@@ -219,8 +219,8 @@ module VCAP::CloudController
     end
 
     def self.setup_routes
-      get    "/services/v1/offerings/:label/handles",           :list_handles
-      get    "/services/v1/offerings/:label/:provider/handles", :list_handles
+      get    "/services/v1/offerings/:label_and_version/handles",           :list_handles
+      get    "/services/v1/offerings/:label_and_version/:provider/handles", :list_handles
       get    "/services/v1/offerings/:label/:provider",         :get
       get    "/services/v1/offerings/:label",                   :get
       delete "/services/v1/offerings/:label",                   :delete

spec/api/legacy_service_gateway_spec.rb
@@ -245,7 +245,7 @@ module VCAP::CloudController
             :provider => "core",
           )
           Models::ServiceAuthToken.create(
-            :label => "foo-bar",
+            :label => "foo",
             :provider => "core",
             :token => "foobar",
           )
@@ -256,7 +256,7 @@ module VCAP::CloudController
             :provider => "test",
           )
           Models::ServiceAuthToken.create(
-            :label => "foo-bar",
+            :label => "foo",
             :provider => "test",
             :token => "footest",
           )